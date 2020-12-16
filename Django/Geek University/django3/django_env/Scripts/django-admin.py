b98a63e6b300499ebf57deccf836e78ceb470a02
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2020-12-15 23:21:27 -0300

    estudos django e js

---
+++ Django/Geek University/django3/django_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ga_fi\OneDrive\Documentos\Projetos\WEB\Django\Geek University\django3\django_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
